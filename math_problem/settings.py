7574458b589eb60b34d12d01794f3d39ca80ecea
xiaoyu2006 <1783250036@qq.com>
2018-12-27 22:25:58 +0800

    set

--- math_problem/settings.py
+++ math_problem/settings.py
@@ -111,15 +111,15 @@ BOOTSTRAP3={
 }
 
 # Settings for Heroku
-# DEBUG = False
-# import dj_database_url
-# DATABASES={
-#    'default': dj_database_url.config(default='postgres://localhost')
-# }
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# ALLOWED_HOSTS = ['*']
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = 'staticfiles'
-# STATICFILES_DIRS=(
-#     os.path.join(BASE_DIR, 'static'),
-# )
+DEBUG = False
+import dj_database_url
+DATABASES={
+   'default': dj_database_url.config(default='postgres://localhost')
+}
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, 'static'),
+)
