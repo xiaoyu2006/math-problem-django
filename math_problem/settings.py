4795e4355e56677681521f20545773f7299409c1
xiaoyu2006 <1783250036@qq.com>
2018-12-29 23:15:36 +0800

    Setting for uploading images to Heroku: save on Qiniu

--- math_problem/settings.py
+++ math_problem/settings.py
@@ -112,15 +112,15 @@ BOOTSTRAP3={
 }
 
 # Settings for Heroku
-DEBUG = True
-import dj_database_url
-DATABASES={
-   'default': dj_database_url.config(default='postgres://localhost')
-}
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, 'static'),
-)
+# DEBUG = True
+# import dj_database_url
+# DATABASES={
+#    'default': dj_database_url.config(default='postgres://localhost')
+# }
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# ALLOWED_HOSTS = ['*']
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATICFILES_DIRS=(
+#     os.path.join(BASE_DIR, 'static'),
+# )
--- problem/views.py
+++ problem/views.py
@@ -1,9 +1,43 @@
+from os.path import abspath
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from .models import Problem
 from .forms import ProbForm, SolveForm
+
+# Some functions
+def uploadimg(img_name):
+    from qiniu import Auth, put_file, etag
+    import qiniu.config
+    access_key='hf_OTa2nV0Taho84tpokRzn6Gm58mrjwu5QOPHGw'
+    secret_key='-uwYx2tXAsUpSssc5YRH026F8qlzt_h5qCpC3OFK'
+    q=Auth(access_key, secret_key)
+    bucket_name='idup'
+    key='math-problem-proj/'+img_name.split('/')[-1]
+    # 2592000s refers to 30d
+    token=q.upload_token(bucket_name, key, 2592000)
+    localfile=img_name
+    ret,info=put_file(token, key, localfile)
+    print(info)
+    assert ret['key']==key
+    assert ret['hash']==etag(localfile)
+
+def getimg(img_name):
+    # import requests
+    from qiniu import Auth
+    access_key='hf_OTa2nV0Taho84tpokRzn6Gm58mrjwu5QOPHGw'
+    secret_key='-uwYx2tXAsUpSssc5YRH026F8qlzt_h5qCpC3OFK'
+    q = Auth(access_key, secret_key)
+    key='math-problem-proj/'+img_name.split('/')[-1]
+    base_url = 'http://%s/%s' % ('assets.nemoworks.info', key)
+    private_url = q.private_download_url(base_url, expires=2592000)
+    # print(private_url)
+    # r = requests.get(private_url)
+    # assert r.status_code == 200
+    return private_url
+
 
 # Create your views here.
 
@@ -19,6 +53,8 @@ def show_prob(request, id):
     prob=get_object_or_404(Problem, id=id)
     solves=prob.solve_set.all().order_by('-date_added')
     context={'prob': prob, 'solves': solves}
+    if prob.img:
+        context['img_url']=getimg(prob.img.name)
     return render(request, 'problem.html', context=context)
 
 def new_prob(request):
@@ -27,7 +63,9 @@ def new_prob(request):
     else:
         form=ProbForm(request.POST, request.FILES)
         if form.is_valid():
+            probobj=form.save(commit=False)
             form.save()
+            uploadimg(abspath('media/%s'%(probobj.img.name)))
             return HttpResponseRedirect(
                 reverse('prob:probs')
             )
